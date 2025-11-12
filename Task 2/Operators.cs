d750e3eb84112457861ac22a2fdb5211e56a1167
bibek0-0 <luitelbibek0@gmail.com>
2025-11-12 19:53:23 +0545

    Task 2 Completed

---
+++ Task 2/Operators.cs
@@ -0,0 +1,22 @@
+﻿namespace Task_2
+{
+    internal class NullOperations
+    {
+        public void PerformNullChecks()
+        {
+            //Declare and assign null
+            string username = null;
+
+            //Ternary operator to check null
+            string message = (username == null) ? "Username is not available" : username;
+            Console.WriteLine(message);
+
+            //Null-Coalescing Operator (??)
+            Console.WriteLine(username ?? "Username is not available");
+
+            //Null-Coalescing Assignment Operator (??=)
+            username ??= "GuestUser";
+            Console.WriteLine($"Updated username: {username}");
+        }
+    }
+}
--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            NullOperations ops = new NullOperations();
+            ops.PerformNullChecks();
         }
     }
 }
