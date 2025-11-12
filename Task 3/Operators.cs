39f23847add78cbf778691f476fa1e659fd1abc6
bibek0-0 <luitelbibek0@gmail.com>
2025-11-12 20:16:54 +0545

    Task 3 Completed

---
+++ Task 3/Operators.cs
@@ -0,0 +1,26 @@
+﻿namespace Task_3
+{
+    internal class Operators
+    {
+        public void CheckAgeCategory()
+        {
+            Console.Write("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            // Using if-else-if
+            if (age < 13)
+            {
+                Console.WriteLine("Child");
+            }
+            else if (age >= 13 && age <= 19)
+            {
+                Console.WriteLine("Teenager");
+            }
+            else
+            {
+                Console.WriteLine("Adult");
+            }
+
+        }
+    }
+}
--- Task 3/Program.cs
+++ Task 3/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Operators ops = new Operators();
+            ops.CheckAgeCategory();
         }
     }
 }
